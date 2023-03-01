6ceab2106228f701581f3743a5d8e96404277541
NPSoftware68 <thanhtra.sqcb@icloud.com>
2023-03-01 11:15:37 +0700

    Initial Commit

---
+++ FoodPin/View/SafariView.swift
@@ -0,0 +1,22 @@
+//
+//  SafariView.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 24/10/2022.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    var url: URL
+    
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
+        
+    }
+}
